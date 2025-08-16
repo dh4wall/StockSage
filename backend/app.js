ca5f1431a2dc5d901d3c04394fa0e11278aac93d
dh4wall <dhawalshinde14@gmail.com>
2025-08-16 19:20:49 +0530

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const stockRoutes = require('./routes/stockRoutes');
 
 const app = express();
-const port = process.env.port ;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
